75ae62c5b1b1e84f7d422c6470f43bdb96454a95
nerocrux <nerocrux@gmail.com>
2013-06-18 03:06:36 +0900

    using cmd module according to http://code.activestate.com/recipes/280500-console-built-with-cmd-object/

--- __main__.py
+++ __main__.py
@@ -1,18 +1,86 @@
 from src.wayterm import Wayterm
+import os
+import cmd
+import readline
 
-if __name__ == "__main__":
+class Console(cmd.Cmd):
+
+    def __init__(self):
+        cmd.Cmd.__init__(self)
+        self.prompt = "wayterm > "
+
+    ## Command definitions ##
+    def do_hist(self, args):
+        """Print a list of commands that have been entered"""
+        print self._hist
+
+    def do_exit(self, args):
+        """Exits from the console"""
+        return -1
+
+    ## Command definitions to support Cmd object functionality ##
+    def do_EOF(self, args):
+        """Exit on system end of file character"""
+        return self.do_exit(args)
+
+    def do_shell(self, args):
+        """Pass command to a system shell when line begins with '!'"""
+        os.system(args)
+
+    def do_help(self, args):
+        """Get help on commands
+           'help' or '?' with no arguments prints a list of commands for which help is available
+           'help <command>' or '? <command>' gives help on <command>
+        """
+        ## The only reason to define this method is for the help text in the doc string
+        cmd.Cmd.do_help(self, args)
+
+    ## Override methods in Cmd object ##
+    def preloop(self):
+        """Initialization before prompting user for commands.
+           Despite the claims in the Cmd documentaion, Cmd.preloop() is not a stub.
+        """
+        cmd.Cmd.preloop(self)   ## sets up command completion
+        self._hist    = []      ## No history yet
+        self._locals  = {}      ## Initialize execution namespace for user
+        self._globals = {}
+
+    def postloop(self):
+        """Take care of any unfinished business.
+           Despite the claims in the Cmd documentaion, Cmd.postloop() is not a stub.
+        """
+        cmd.Cmd.postloop(self)   ## Clean up command completion
+        print "Exiting..."
+
+    def precmd(self, line):
+        """ This method is called after the line has been input but before
+            it has been interpreted. If you want to modifdy the input line
+            before execution (for example, variable substitution) do it here.
+        """
+        self._hist += [ line.strip() ]
+        return line
+
+    def postcmd(self, stop, line):
+        """If you want to stop the console, return something that evaluates to true.
+           If you want to do some post command processing, do it here.
+        """
+        return stop
+
+    def emptyline(self):
+        """Do nothing on empty input line"""
+        pass
+
+    def default(self, line):
+        """Called on an input line when the command prefix is not recognized.
+           In that case we execute the line as Python code.
+        """
+        try:
+            wayterm.call(line.split('\\'))
+        except Exception, e:
+            print e.__class__, ":", e
+
+if __name__ == '__main__':
     wayterm = Wayterm()
     wayterm._init_print()
-    while True:
-        try:
-            command = raw_input('wayterm > ')
-            if len(command) == 0:
-                pass
-            if command.lower() == 'exit':
-                print 'exiting...'
-                exit()
-            else:
-                wayterm.call(command.split('\\'))
-        except RuntimeError:
-            print 'Error, exiting...'
-            exit()
+    console = Console()
+    console . cmdloop()
